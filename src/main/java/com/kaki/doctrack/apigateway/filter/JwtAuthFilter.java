a050c0a484ad59747b49a63bfb0c64f54e97a662
Joaquin Navarro <alfian1991@gmail.com>
2024-06-19 13:53:26 -0400

    fix backend authentication and authorization

--- src/main/java/com/kaki/doctrack/apigateway/filter/JwtAuthFilter.java
+++ src/main/java/com/kaki/doctrack/apigateway/filter/JwtAuthFilter.java
@@ -7,7 +7,6 @@ import org.springframework.cloud.gateway.filter.GatewayFilter;
 import org.springframework.cloud.gateway.filter.factory.AbstractGatewayFilterFactory;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
-import org.springframework.http.HttpStatusCode;
 import org.springframework.stereotype.Component;
 import org.springframework.web.reactive.function.client.WebClient;
 import org.springframework.web.server.ResponseStatusException;
@@ -20,20 +19,16 @@ public class JwtAuthFilter extends AbstractGatewayFilterFactory<JwtAuthFilter.Co
 
     private final Logger logger = LoggerFactory.getLogger(JwtAuthFilter.class);
 
-    private WebClient.Builder webClientBuilder;
+    private final WebClient.Builder webClientBuilder;
 
-    public JwtAuthFilter() {
+    public JwtAuthFilter(WebClient.Builder webClientBuilder) {
         super(Config.class);
-        webClientBuilder = WebClient.builder();
+        this.webClientBuilder = webClientBuilder;
     }
 
     @Override
     public GatewayFilter apply(Config config) {
         return (exchange, chain) -> {
-            if (!exchange.getRequest().getHeaders().containsKey(HttpHeaders.AUTHORIZATION)) {
-                return Mono.error(new ResponseStatusException(HttpStatus.UNAUTHORIZED, "Missing Authorization Header"));
-            }
-
             String authHeader = Objects.requireNonNull(exchange.getRequest().getHeaders().get(HttpHeaders.AUTHORIZATION)).get(0);
 
             if (authHeader == null || !authHeader.startsWith("Bearer ")) {
@@ -46,12 +41,11 @@ public class JwtAuthFilter extends AbstractGatewayFilterFactory<JwtAuthFilter.Co
                     .get()
                     .uri(config.getAuthValidationUrl() + "?token=" + token)
                     .retrieve()
-                    .onStatus(HttpStatusCode::isError, response -> Mono.error(new ResponseStatusException(response.statusCode(), "Invalid Token")))
                     .bodyToMono(Void.class)
-                    .then(chain.filter(exchange))
+                    .then(Mono.defer(() -> chain.filter(exchange)))
                     .doOnError(e -> {
-                        // Log error here
-                        System.out.println("Authorization error: " + e.getMessage());
+                        logger.error("Error validating token: {}", e.getMessage());
+                        exchange.getResponse().setStatusCode(HttpStatus.UNAUTHORIZED);
                     });
         };
     }
