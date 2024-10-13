dbe1cb6f17479ba6c114c268e86df2f637bc2d0a
Joaquin Navarro <alfian1991@gmail.com>
2024-10-13 17:27:48 -0400

    adding doctrack-frontend

--- src/main/java/com/kaki/doctrack/apigateway/filter/JwtAuthFilter.java
+++ src/main/java/com/kaki/doctrack/apigateway/filter/JwtAuthFilter.java
@@ -7,8 +7,10 @@ import org.springframework.cloud.gateway.filter.GatewayFilter;
 import org.springframework.cloud.gateway.filter.factory.AbstractGatewayFilterFactory;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.server.reactive.ServerHttpRequest;
 import org.springframework.stereotype.Component;
 import org.springframework.web.reactive.function.client.WebClient;
+import org.springframework.web.server.ServerWebExchange;
 import reactor.core.publisher.Mono;
 
 @Component
@@ -41,7 +43,26 @@ public class JwtAuthFilter extends AbstractGatewayFilterFactory<JwtAuthFilter.Co
                     .uri(config.getAuthValidationUrl() + "?token=" + token)
                     .exchangeToMono(clientResponse -> {
                         if (clientResponse.statusCode().is2xxSuccessful()) {
-                            return chain.filter(exchange); // Token is valid, proceed with the request
+                            logger.info("Token is valid");
+
+                            return clientResponse.bodyToMono(UserInfo.class)
+                                    .flatMap(userInfo -> {
+                                        // Remove Authorization header
+                                        // Build a new ServerHttpRequest with the updated headers
+                                        ServerHttpRequest mutatedRequest = exchange.getRequest().mutate()
+                                                .headers(httpHeaders -> httpHeaders.remove(HttpHeaders.AUTHORIZATION))
+                                                .header("X-User-Id", userInfo.userId().toString())
+                                                .header("X-User-Role", userInfo.role())
+                                                .header("X-User-Name", userInfo.username())
+                                                .build();
+
+                                        // Update the ServerWebExchange with the mutated request
+                                        ServerWebExchange mutatedExchange = exchange.mutate()
+                                                .request(mutatedRequest)
+                                                .build();
+
+                                        return chain.filter(mutatedExchange);
+                                    });
                         } else {
                             logger.error("Token validation failed with status: {}", clientResponse.statusCode());
                             exchange.getResponse().setStatusCode(HttpStatus.UNAUTHORIZED);
---
+++ src/main/java/com/kaki/doctrack/apigateway/filter/UserInfo.java
@@ -0,0 +1,4 @@
+package com.kaki.doctrack.apigateway.filter;
+
+public record UserInfo(String username, Long userId, String role) {
+}
